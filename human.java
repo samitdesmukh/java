0fc53b06eb837a661d506dac93fd17f69e915b33
samit <desmukhkumarsamit@gmail.com>
2024-09-28 23:37:01 +0530

    link class files in java

---
+++ human.java
@@ -0,0 +1,26 @@
+public class human{
+    private String name;
+    private int age;
+
+    public String getName()
+    {
+        return name;
+    }
+    public void setName(String name)
+    {
+        this.name=name;
+    }
+    public int getAge()
+    {
+        return age;
+    }
+    public void setAge(int age)
+    {
+        this.age=age;
+    }
+    public void show()
+    {
+        System.out.println("Name:"+ name +",Age: "+age);
+    }
+    
+}
---
+++ man1.java
@@ -0,0 +1,12 @@
+public class man1{
+    public static void main(String args[])
+    {
+        human man2 = new human();
+        man2.setName("samit");
+        man2.setAge(25);
+        System.out.println("Name:"+man2.getName());
+        System.out.println("Age:"+man2.getAge());
+        
+
+    }
+}
